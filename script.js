fbe2280ddc6050bf2fe4e73d599c7b0672d176d6
Meet Kothari <meet.kothari@blazeclan.com>
2022-12-25 21:12:51 +0530

    New Update

--- script.js
+++ script.js
@@ -19,17 +19,17 @@ const getWeather = (city) => {
 
       //   cloud_pct.innerHTML = response.cloud_pct; compromised
       temp.innerHTML = response.temp;
-      temp2.innerHTML = response.temp;
-      feels_like.innerHTML = response.feels_like;
-      humidity.innerHTML = response.humidity;
-      humidity2.innerHTML = response.humidity;
+      // temp2.innerHTML = response.temp;
+      // feels_like.innerHTML = response.feels_like;
+      // humidity.innerHTML = response.humidity;
+      // humidity2.innerHTML = response.humidity;
       min_temp.innerHTML = response.min_temp;
       max_temp.innerHTML = response.max_temp;
-      wind_speed.innerHTML = response.wind_speed;
-      wind_speed2.innerHTML = response.wind_speed;
-      wind_degrees.innerHTML = response.wind_degrees;
-      sunrise.innerHTML = response.sunrise;
-      sunset.innerHTML = response.sunset;
+      // wind_speed.innerHTML = response.wind_speed;
+      // wind_speed2.innerHTML = response.wind_speed;
+      // wind_degrees.innerHTML = response.wind_degrees;
+      // sunrise.innerHTML = response.sunrise;
+      // sunset.innerHTML = response.sunset;
     })
     .catch((err) => console.error(err));
 };
